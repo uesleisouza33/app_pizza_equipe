261e718e1f3b3cd28fc402a4c4b4f8b7c1d12806
jair Hyan Fernandes da Silva <jair_h_silva@estudante.sesisenai.org.br>
2026-02-24 09:13:58 -0300

    fazendo a receita2

---
+++ models/receita.js
@@ -0,0 +1,12 @@
+export default class Receita{
+    constructor(sal, agua, fermento){
+        this.sal = sal,
+        this.agua = agua,
+        this.fermento = fermento
+    }
+    calcularIngredientes(){
+        this.sal = 10
+        this.agua = 20
+        this.fermento = 30
+    }
+}
